bd5e77f7cc2d46f3b7987ae421a4e751538bbc06
Giacomo_Meloni <giacomo.meloni.mail@gmail.com>
2019-06-24 22:29:33 +0200

    Started DAG Node Class - Created Delauney triangulation Class

---
+++ GAS_2019_Delaunay/algorithms/delauneytriangulation.cpp
@@ -0,0 +1,6 @@
+#include "delauneytriangulation.h"
+
+DelauneyTriangulation::DelauneyTriangulation()
+{
+
+}
---
+++ GAS_2019_Delaunay/algorithms/delauneytriangulation.h
@@ -0,0 +1,11 @@
+#ifndef DELAUNEYTRIANGULATION_H
+#define DELAUNEYTRIANGULATION_H
+
+
+class DelauneyTriangulation
+{
+public:
+    DelauneyTriangulation();
+};
+
+#endif // DELAUNEYTRIANGULATION_H
